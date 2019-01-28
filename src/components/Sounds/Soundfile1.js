9143d6788b118bd41356d21e1682c4ca1d7f84dd
kevin <k.drago57@gmail.com>
2019-01-28 23:09:45 +1000

    audio fixes

--- src/components/Sounds/Soundfile1.js
+++ src/components/Sounds/Soundfile1.js
@@ -4,63 +4,63 @@ const soundNoteAlpha = [
     keyTrigger: "Q",
     name: "Hot Damn",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Captain%20Holt/HOT%20DAMN%20.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Captain%20Holt/HOT%20DAMN%20.mp3"
   },
   {
     keycode: 87,
     keyTrigger: "W",
     name: "Unleash hell",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Captain Holt/Let's unleash hell.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Captain Holt/Let's unleash hell.mp3"
   },
   {
     keycode: 69,
     keyTrigger: "E",
     name: "Marco Polo",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Captain Holt/Marco polo.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Captain Holt/Marco polo.mp3"
   },
   {
     keycode: 65,
     keyTrigger: "A",
     name: "Unleash the Beast",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Captain Holt/Unleash the beast.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Captain Holt/Unleash the beast.mp3"
   },
   {
     keycode: 83,
     keyTrigger: "S",
     name: "Velvet thunder",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Captain Holt/Velvet thunder.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Captain Holt/Velvet thunder.mp3"
   },
   {
     keycode: 68,
     keyTrigger: "D",
     name: "Release your sweets",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Terry Jeffords/Release your sweets.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Terry Jeffords/Release your sweets.mp3"
   },
   {
     keycode: 90,
     keyTrigger: "Z",
     name: "Yoghurt",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Terry Jeffords/Yoghurt, me too.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Terry Jeffords/Yoghurt, me too.mp3"
   },
   {
     keycode: 88,
     keyTrigger: "X",
     name: "Wwwhaaat",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Terry Jeffords/Whhatt.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Terry Jeffords/Whhatt.mp3"
   },
   {
     keycode: 67,
     keyTrigger: "C",
     name: "This is too long",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Terry Jeffords/This is taking too long.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Terry Jeffords/This is taking too long.mp3"
   }
 ];
 
--- src/components/Sounds/Soundfile2.js
+++ src/components/Sounds/Soundfile2.js
@@ -4,63 +4,63 @@ const soundNoteBeta = [
     keyTrigger: "Q",
     name: "Dry Boy",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Charles Boyle/A dry boy is a smart boy.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Charles Boyle/A dry boy is a smart boy.mp3"
   },
   {
     keycode: 87,
     keyTrigger: "W",
     name: "Pina collada",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Charles Boyle/Pina collada.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Charles Boyle/Pina collada.mp3"
   },
   {
     keycode: 69,
     keyTrigger: "E",
     name: "Nikolaj",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Charles Boyle/Nikolaj 6.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Charles Boyle/Nikolaj 6.mp3"
   },
   {
     keycode: 65,
     keyTrigger: "A",
     name: "EEEEE",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Jake Peralta/EEEE EEEE EEEE EEEE.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Jake Peralta/EEEE EEEE EEEE EEEE.mp3"
   },
   {
     keycode: 83,
     keyTrigger: "S",
     name: "Guilty cool",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Jake Peralta/Guilty cool.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Jake Peralta/Guilty cool.mp3"
   },
   {
     keycode: 68,
     keyTrigger: "D",
     name: "Good and Prof",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Jake Peralta/It will be good and professional.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Jake Peralta/It will be good and professional.mp3"
   },
   {
     keycode: 90,
     keyTrigger: "Z",
     name: "No Spoilees",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Jake Peralta/No spoilees.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Jake Peralta/No spoilees.mp3"
   },
   {
     keycode: 88,
     keyTrigger: "X",
     name: "Was it cool?",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Jake Peralta/Was it cool it felt very cool.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Jake%20Peralta/Was%20it%20cool%20it%20felt%20very%20cool.mp3"
   },
   {
     keycode: 67,
     keyTrigger: "C",
     name: "Head and muffin",
     url:
-      "https://holt-soundboard.github.io/assets/sounds/Charles Boyle/Head and muffin.mp3"
+      "https://raw.githubusercontent.com/holt-soundboard/temp/master/copyright/legal_docs/more/sounds/Charles Boyle/Head and muffin.mp3"
   }
 ];
 
